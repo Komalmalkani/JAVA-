bd046c9715a338d36cf967fc484aa2f1d3bf2dca
Komal Malkani <malkanianita15@gmail.com>
2024-09-27 17:36:44 +0530

    Adding new code

--- class1.java
+++ class1.java
@@ -1,15 +1,24 @@
 // __________BOILERPLATE CODE_____________
-public class class1{
+// public class class1{
+//     public static void main(String args[]){
+//         System.out.print("Hello World");
+//         System.out.println("1  2  3  4  5");
+//         // ln => Basically gives line ka space (next thing will be in next line)
+//         System.out.println("Hello World");
+//         System.out.println("Hello World");
+//         System.out.println("Hello World");
+//         System.out.println("Hello World");
+//         System.out.print("Hello ,\n I am a Komal Malkani");
+//     }
+// }
+
+
+public class main{
     public static void main(String args[]){
-        System.out.print("Hello World");
-        System.out.println("1  2  3  4  5");
-        // ln => Basically gives line ka space (next thing will be in next line)
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.print("Hello ,\n I am a Komal Malkani");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }
-
 
