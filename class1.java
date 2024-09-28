65b12c419ae1d35909d96c523e93221d247c3911
Komal Malkani <malkanianita15@gmail.com>
2024-09-28 20:01:14 +0530

    Adding new code

--- class1.java
+++ class1.java
@@ -1,4 +1,11 @@
 // __________BOILERPLATE CODE_____________
+public class class1{
+    public static void main(String args[]){
+    }
+}
+
+
+
 // public class class1{
 //     public static void main(String args[]){
 //         System.out.print("Hello World");
