05ef9cc264699589aae31df14cffd1ad6f7f15e0
Komal Malkani <malkanianita15@gmail.com>
2024-09-28 20:43:11 +0530

    Adding new code

--- class2.java
+++ class2.java
@@ -1,5 +1,18 @@
+// _____________Previous class Revision__________________
+// public class class2 {
+//     public static void main(String args[]){
+//         System.out.print("komal is here");
+//     }
+// }
+
+// __________Variables in JAVA_____________
+
 public class class2 {
-    public static void main(String args[]){
-        System.out.print("komal is here");
+        public static void main(String args[]){
+            int a = 5;
+            int b = 5;
+            System.out.println("a");
+            System.out.println(a);
+        }
     }
-}
+
