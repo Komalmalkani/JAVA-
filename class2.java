fb84ca65b566d51aab3c24105bbd06b7c408ce8b
Komal Malkani <malkanianita15@gmail.com>
2024-09-28 21:03:36 +0530

    Adding new code

--- class2.java
+++ class2.java
@@ -7,16 +7,36 @@
 
 // __________Variables in JAVA_____________
 
+// public class class2 {
+//         public static void main(String args[]){
+//             int a = 5;
+//             int b = 5;
+//             System.out.println("a");
+//             System.out.println(a);
+//            a = 50;
+//            System.out.println(a);
+//            a=b;
+//            System.out.println(a);
+//         }
+//     }
+
+
+// ____________Data Types In Java__________
+
 public class class2 {
         public static void main(String args[]){
-            int a = 5;
-            int b = 5;
-            System.out.println("a");
-            System.out.println(a);
-           a = 50;
-           System.out.println(a);
-           a=b;
-           System.out.println(a);
+            byte b = 8;
+            System.out.println(b);
+            char ch = 'a';
+            System.out.println(ch);
+            boolean var = false;
+            float num = 10.5;
+            int number = 25;
+            // long => int with long number
+            // short => int with short number;
+            // double => float with long number;
+            
+
+
         }
     }
-
