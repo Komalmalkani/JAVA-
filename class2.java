add8dde896b621ce64902d39b924fa071f71c0f9
Komal Malkani <malkanianita15@gmail.com>
2024-09-29 09:14:54 +0530

    new change

--- class2.java
+++ class2.java
@@ -23,27 +23,49 @@
 
 // ____________Data Types In Java__________
 
-public class class2 {
-        public static void main(String args[]){
-            byte b = 8;
-            System.out.println(b);
-            char ch = 'a';
-            System.out.println(ch);
-            boolean var = false;
-            System.out.println( boolean );
-            float num = 10.5;
-            System.out.println(float);
-            int number = 25;
-            System.out.println(int);
-            // long => int with long number
-            long no  =529753454675;
-            System.out.println(long);
+// public class class2 {
+//         public static void main(String args[]){
+//             byte b = 8;
+//             System.out.println(b);
+//             char ch = 'a';
+//             System.out.println(ch);
+//             boolean var = false;
+//             System.out.println( var);
+//             float num = 10.5f;
+//             System.out.println(num);
+//             int number = 25;
+//             System.out.println(number);
+//             // long => int with long number
+//             long no  = 753454675;
+//             System.out.println(no);
         
             
-            // short => int with short number;
-            // double => float with long number;
+//             // short => int with short number;
+//             short sht = 28;
+//             System.out.println(sht);
+//             // double => float with long number;
+//         }
+//     }
 
 
+// ____________COMMENTS IN JAVA_____________
+// ----single-line------
+// HI, I M KOMAL MALKANI
 
-        }
-    }
+// ----Multi-line------
+
+/*HI,
+THERE,
+HOW ARE UH?
+I am learning java */
+
+
+// --------SUM OF A & B ---------
+public class class2 {
+public static void main(String args[]){
+    int a = 10;
+    int b = 10;
+    int sum = a+b;
+    System.out.println(sum);
+}
+}
