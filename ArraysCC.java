73d3c315623220bdbfd973bf8f7222aabbf762d9
Komal Malkani <malkanianita15@gmail.com>
2024-10-07 21:01:24 +0530

    some changes

--- ArraysCC.java
+++ ArraysCC.java
@@ -1,4 +1,15 @@
+ // ________DEFINITION OF ARRAY_____________
+     // list of elements of the same type placed in a contiguous memory location
+       
+ // ______OPERATIONS IN ARRAY_____________
+
+        // Create
+        // Input
+        // Output
+        // Update
 public class ArraysCC {
-    
+    public static void main(String args[]) {
+        
+    }
 }
 
